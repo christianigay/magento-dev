76a83f50b2e56bb337fbd85ec3e7f2ca8a9b0835
christian igay <christian.igay@gmail.com>
2023-10-20 13:29:18 +0000

    product attribute

--- app/code/Mageplaza/HelloWorld/Setup/InstallData.php
+++ app/code/Mageplaza/HelloWorld/Setup/InstallData.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Mageplaza\HelloWorld\Setup;
 
 use Magento\Eav\Setup\EavSetup;
@@ -7,54 +6,43 @@ use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\InstallDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
-use Magento\Eav\Model\Config;
-use Magento\Customer\Model\Customer;
 
 class InstallData implements InstallDataInterface
 {
 	private $eavSetupFactory;
-	public $eavConfig;
 
-	public function __construct(EavSetupFactory $eavSetupFactory, Config $eavConfig)
+	public function __construct(EavSetupFactory $eavSetupFactory)
 	{
 		$this->eavSetupFactory = $eavSetupFactory;
-		$this->eavConfig = $eavConfig;
 	}
-
+	
 	public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
 	{
 		$eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
 		$eavSetup->addAttribute(
-			\Magento\Customer\Model\Customer::ENTITY,
+			\Magento\Catalog\Model\Product::ENTITY,
 			'sample_attribute',
 			[
-				'type' 				=> 'varchar',
-				'label' 			=> 'Sample Attribute',
-				'input' 			=> 'text',
-				'required' 			=> false,
-				'visible'			=> true,
-				'user_defined'		=> true,
-				'position'			=> 999,
-				'system'			=> 0
+				'type' => 'text',
+				'backend' => '',
+				'frontend' => '',
+				'label' => 'Sample Atrribute',
+				'input' => 'text',
+				'class' => '',
+				'source' => '',
+				'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+				'visible' => true,
+				'required' => true,
+				'user_defined' => false,
+				'default' => '',
+				'searchable' => false,
+				'filterable' => false,
+				'comparable' => false,
+				'visible_on_front' => false,
+				'used_in_product_listing' => true,
+				'unique' => false,
+				'apply_to' => ''
 			]
 		);
-
-		$sampleAttribute = $this->eavConfig->getAttribute(Customer::ENTITY, 'sample_attribute');
-
-		$sampleAttribute->setData(
-			'used_in_forms',
-			['adminhtml_customer']
-		);
-
-		$sampleAttribute->save();
-		// $data = [
-		// 	'name'         => "How to Create SQL Setup Script in Magento 2",
-		// 	'post_content' => "In this article, we will find out how to install and upgrade sql script for module in Magento 2. When you install or upgrade a module, you may need to change the database structure or add some new data for current table. To do this, Magento 2 provide you some classes which you can do all of them.",
-		// 	'url_key'      => '/magento-2-module-development/magento-2-how-to-create-sql-setup-script.html',
-		// 	'tags'         => 'magento 2,mageplaza helloworld',
-		// 	'status'       => 1
-		// ];
-		// $post = $this->_postFactory->create();
-		// $post->addData($data)->save();
 	}
 }
