d81eafbd759d92b3c8f282c70565ee44d65f6c29
christian igay <christian.igay@gmail.com>
2023-10-20 10:17:58 +0000

    add indexers

---
+++ app/code/Mageplaza/HelloWorld/Model/Indexer/Test.php
@@ -0,0 +1,26 @@
+<?php
+namespace Mageplaza\HelloWorld\Model\Indexer;
+
+class Test implements \Magento\Framework\Indexer\ActionInterface, \Magento\Framework\Mview\ActionInterface
+{
+
+    public function execute($ids)
+    {
+
+    }
+
+    public function executeFull()
+    {
+
+    }
+
+    public function executeList(array $ids)
+    {
+        
+    }
+
+    public function executeRow($id)
+    {
+        
+    }
+}
